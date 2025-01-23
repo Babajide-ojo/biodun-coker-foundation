1a443286e288f40467fa395dad6053c5327b773f
babajide.ojo <ojobabajide629@gmail.com>
2025-01-24 00:21:45 +0100

    chore

--- app/about/page.js
+++ app/about/page.js
@@ -1,13 +1,16 @@
+"use client"
 import React from "react";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 const PDFViewer = () => {
   return (
-    <div style={{ height: "100vh" }}>
-      <iframe
-        src="https://res.cloudinary.com/dyb8cgrxm/image/upload/v1737673898/p3z0gxs4wnnwhpngtfbn.pdf"
-        title="PDF Viewer"
-        style={{ width: "100%", height: "100%", border: "none" }}
-      />
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+        <Viewer
+          fileUrl="https://res.cloudinary.com/dyb8cgrxm/image/upload/v1737673898/p3z0gxs4wnnwhpngtfbn.pdf"
+        />
+      </Worker>
     </div>
   );
 };
